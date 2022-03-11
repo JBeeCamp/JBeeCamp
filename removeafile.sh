0bf67eca316a665876706e90e988966c58b54af7
JBeeCamp <jbeecamp@gmail.com>
2022-03-10 22:58:05 -0500

    [A[A[B[B[A[Bstop

--- removeafile.sh
+++ removeafile.sh
@@ -1,24 +1,24 @@
 #!usr/bin/bash
 
-echo $'Lets Remove That File! Enter stop at anytime to quit out! \n'
+echo $'Lets Remove That File! \n\nEnter stop at anytime to exit script... \n'
 
 while [ true ]; do
 
-        read  -p $'Please enter file to be removed: \n' input
+        read  -p $'Please enter file to be removed: \n\n' input
 
 	if [[ ${input^^} == "STOP" || $input == "stop" ]]; then
                 break
         elif [[ -z $input ]]; then
-                echo "Input empty, please try again."
+                echo $'Input empty, please try again. \n\n'
         fi
 
 	rm $input
 
-	echo $'Initializing commit and push for remote deletion... \n'
+	echo $'Initializing commit and push for remote deletion... \n\n'
 
-	git commit -a -m $'File deleted! \n'
+	git commit -a -m $'File deleted! \n\n'
 
 	git push
 
-	echo $'Change has been pushed! \n'
+	echo $'Change has been pushed! \n\n'
 done
