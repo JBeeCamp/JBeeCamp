a22f56054d7bbd992260a0f814601047445d0e2e
JBeeCamp <jbeecamp@gmail.com>
2022-03-10 22:56:22 -0500

    [A[B[A[A[A[A[B[B[B[B[B[B[B[B[B[B[B[C[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[B[A[A[A[A[A[A[A[A[A[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[A[BUpdated my script with better readability.

--- pushafile.sh
+++ pushafile.sh
@@ -1,33 +1,35 @@
 #!usr/bin/bash
 
+echo $'Lets git that file and push it! \n\nEnter stop at anytime to exit script... \n'
+
 while [ true ]; do
 
-read -p $'Please enter file name: \n' filename
+read -p $'Please enter file name: \n\n' filename
 
 	if [[ $filename^^ == "STOP" || $filename == "stop" ]]; then
                 break
 
         elif [[ -z $filename ]]; then
-                echo "Input empty, please input file name."
+                echo $'Input empty, please input file name... \n\n'
 	else
 
 		git add $filename
 	fi
 
-read -p $'Please enter commit message that will be pushed: \n' message
+read -p $'Please enter commit message that will be pushed: \n\n' message
 
 	if [[ $message^^ == "STOP" || $message == "stop" ]]; then
                 break
 
         elif [[ -z $message ]]; then
-                echo "Input empty, please input file name."
+                echo $'Input empty, please input file name... \n\n'
 
 	else
 
 		git commit -m "$message"
 	fi
 
-echo Pushing file to origin.
+echo $'Pushing file to origin... \n\n'
 
 git push
 
