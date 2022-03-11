4317b2a5e290faa5de929a4b7815f71bb16bd6ac
JBeeCamp <jbeecamp@gmail.com>
2022-03-11 10:34:29 -0500

    Updated the script.

--- pushafile.sh
+++ pushafile.sh
@@ -2,18 +2,17 @@
 
 echo $'Lets git that file and push it! \nEnter stop at anytime to exit script... \n'
 
-
 while [ true ]; do
 
 read -p $'Please enter file name: \n' filename
 
-
 	if [[ $filename^^ == "STOP" || $filename == "stop" ]]; then
-                break
+
+               	break
 
         elif [[ -z $filename ]]; then
-                echo $'Input empty, please input file name... \n'
 
+                echo $'Input empty, please input file name... \n'
 	else
 
 		git add $filename
@@ -23,22 +22,22 @@ read -p $'Please enter commit message that will be pushed: \n' message
 
 
 	if [[ $message^^ == "STOP" || $message == "stop" ]]; then
+
                 break
 
         elif [[ -z $message ]]; then
 
-                echo $'Input empty, please input file name... \n'
-
+                echo $'Input empty, please input commit message... \n'
 
 	else
 
 		git commit -m "$message"
 	fi
 
-echo $'Pushing file to origin... \n'
+	echo $'Pushing file to origin... \n'
 
 
-git push
+	git push
 
 done
 
